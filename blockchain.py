f706f44895f25d6a60c59be816b81da039e16c4c
Greyson LaLonde <greyson.r.lalonde@wmich.edu>
2021-07-28 00:37:18 -0400

    Update blockchain.py
    add attrs + genesis & block methods

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,29 @@
+from block import Block
+from random import random
+import hashlib
+
+
 class BlockChain:
     def __init__(self):
-        pass
+        self.chain = []
+        self.current_data = []
+        self.nodes = set()
+        self.genesis()
+        self.nonce = ...
+
+    def genesis(self):
+        self.block(proof=0, prev=0)
+
+    def block(self, proof, prev):
+        block = Block(
+            index=len(self.chain),
+            proof=proof,
+            prev=prev,
+            data=self.current_data,
+            nonce=...
+        )
+        self.current_data = []
+        self.chain.append(block)
+        return block
+
+   
